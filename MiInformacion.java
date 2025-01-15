5b8495bb2a8331aea9248b9a73c9d2b6cc8e02f3
Benjamin Vergara <benjavergara90@gmail.com>
2025-01-15 10:49:45 -0300

    First Commit

---
+++ MiInformacion.java
@@ -0,0 +1,10 @@
+
+public class MiInformacion {
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Benjamin Vergara.");
+        System.out.println("Tengo 22 Años de edad.");
+        System.out.println("Soy de Chile y vivo en la ciudad de Santiago.");
+        System.out.println("Me divierte programar, los videojuegos y la musica.");
+
+    }
+} 
